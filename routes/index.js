ec0f6030cbb418263ef2386b5950c0008f2c4e0a
Felix Seol <felixxseol@gmail.com>
2019-05-21 21:12:06 +0900

    return 5 results, no cache (does it work?) added.

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,11 @@ var request = require("request");
 
 /* GET home page. */
 router.get('/searchPlace', function(req, res, next) {
-    var _keyword = req.query.keyword;
-    var _coordinate = req.query.coordinate;
-    var _radius = req.query.radius;
-    var _x = _coordinate.split(',')[0]
-    var _y = _coordinate.split(',')[1]
+    let _keyword = req.query.keyword;
+    let _coordinate = req.query.coordinate;
+    let _radius = req.query.radius;
+    let _x = _coordinate.split(',')[0]
+    let _y = _coordinate.split(',')[1]
     // console.log(_coordinate[1]);
     console.log("REQ.QUERY.KEYWORD      : " + _keyword);
     console.log("REQ.QUERY.COORDINATE   : " + _coordinate);
@@ -17,17 +17,20 @@ router.get('/searchPlace', function(req, res, next) {
     console.log("REQ.QUERY.X(lon)       : " + _x);
     console.log("REQ.QUERY.Y(lat)       : " + _y);
 
-    var options = { method: 'GET',
+    let options = { method: 'GET',
     url: 'https://dapi.kakao.com/v2/local/search/keyword.json',
-    qs: { y: _y, x: _x, query: _keyword , radius: _radius, size: 10},
+    qs: { y: _y, x: _x, query: _keyword , radius: _radius, size: 5, sort: 'distance'},
     headers:
-        { 'Authorization': 'KakaoAK eddb65c6ffd9f693777d35d9ff66e70e' } };
+        {   'Authorization': 'KakaoAK eddb65c6ffd9f693777d35d9ff66e70e',
+            'Cache-Control': 'no-store'} };
+
 
   request(options, function (error, response, body) {
     if (error) throw new Error(error);
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
 
+    console.log(options);
     console.log(body);
     res.json(body);
   });
